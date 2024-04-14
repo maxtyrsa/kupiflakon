9ff3ff2cefece624add35322f3612f045806b1be
Maxim <tyrsa@doctor.com>
2024-04-14 11:59:03 +0300

    New data

--- kpi.py
+++ kpi.py
@@ -1,2 +1,12 @@
-from func import _kpi_
-_kpi_('/storage/emulated/0/documents/all.csv', '2024-04-01', '2024-04-12')
+from func import _kpi_ #Библиотека с функциями KPI
+from func import _order_ #Библиотека с функциями заказов
+choic = input("Выберите метрику расчета - kpi или orders: ")
+url = input('Введите имя файла csv: ')
+start_date = input('Введите начальную дату в формате "год-месяц-день": ')
+end_date = input('Введите конечную дату в формате "год-месяц-день": ')
+
+if choic == 'kpi':
+    _kpi_(url, start_date, end_date) #Запуск функции KPI
+if choic == 'orders':
+    _order_(url, start_date, end_date) #Запуск функции заказов
+
