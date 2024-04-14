714ee70dc2e915336e89654f10cff8f302f6c2bb
Tyrsa Maxim <37588248+maxtyrsa@users.noreply.github.com>
2024-04-15 00:59:51 +0300

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -2,17 +2,19 @@ from func import _kpi_ #Библиотека с функциями KPI
 from func import _order_ #Библиотека с функциями заказов
 import os
  # Указываем путь к директории
-directory = "/storage/emulated/0/git/Projekt/files/"
+directory = "/workspaces/Projekt/files/"
  # Получаем список файлов
 files = os.listdir(directory)
  # Выводим список файлов
-choic = input("Выберите метрику расчета - kpi или orders: ")
+print('1 - KPI')
+print('2 - Orders')
+choic = input("Выберите метрику расчета: ")
 print(files)
 url = input('Введите имя файла csv: ')
 start_date = input('Введите начальную дату в формате "год-месяц-день": ')
 end_date = input('Введите конечную дату в формате "год-месяц-день": ')
 
-if choic == 'kpi':
+if choic == '1':
     _kpi_(url, start_date, end_date) #Запуск функции KPI
-if choic == 'orders':
+if choic == '2':
     _order_(url, start_date, end_date) #Запуск функции заказов
