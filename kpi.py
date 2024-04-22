ca2ce9ea5ec36d806250aa68d71409651a0f1098
Tyrsa Maxim <37588248+maxtyrsa@users.noreply.github.com>
2024-04-22 23:40:26 +0300

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -1,5 +1,6 @@
 from func import _kpi_ #Библиотека с функциями KPI
 from func import _order_ #Библиотека с функциями заказов
+from func import _time_orders_ #Библиотека примерного времени сбоки заказов
 import os
  # Указываем путь к директории
 directory = "/home/max/git/Projekt/files/"
@@ -8,6 +9,7 @@ files = os.listdir(directory)
  # Выводим список файлов
 print('1 - KPI')
 print('2 - Orders')
+print('3 - Time')
 choic = input("Выберите метрику расчета: ")
 print(files)
 url = input('Введите имя файла csv: ')
@@ -18,3 +20,5 @@ if choic == '1':
     _kpi_(url, start_date, end_date) #Запуск функции KPI
 if choic == '2':
     _order_(url, start_date, end_date) #Запуск функции заказов
+if choic == '3':
+    _time_orders_(h, m, c) #Запуск функции времени сбоки
