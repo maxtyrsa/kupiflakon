1a3a031d3dca684aed58f4a72a5046c5e03c49bf
maxtyrsa <tyrsa@doctor.com>
2024-06-14 16:39:52 +0300

    dump base

--- postgres/jambs.py
+++ postgres/jambs.py
@@ -1,19 +1,21 @@
 from datetime import datetime
+x = int(input())
 list = []
-i = input("Введите id: ")
-p = input("Количество: ")
-d = datetime.now().strftime('%Y-%m-%d')
-print("""Выберите статус:
+for i in range(x):
+	i = input("Введите id: ")
+	p = input("Количество: ")
+	d = datetime.now().strftime('%Y-%m-%d')
+	print("""Выберите статус:
 1 - Ошибка
 2 - Недовложение
 3 - Лишнее
 """)
-j = int(input("Статус: "))
-ones = [' ',  'Ошибка', 'Недовложение', 'Лишнее']
-word = ones[int(j)]
-list.append(f"({i}, '{word}', {p}, '{d}'),")
-print("""INSERT INTO
+	j = int(input("Статус: "))
+	ones = [' ',  'Ошибка', 'Недовложение', 'Лишнее']
+	word = ones[int(j)]
+	list.append(f"({i}, '{word}', {p}, '{d}'),")
+	print("""INSERT INTO
     jambs (id, jamb, place, date)
 VALUES""")
-print(*[item + '\n' for item in list])
-    
+	print(*[item + '\n' for item in list])
+	continue
--- postgres/repeated.py
+++ postgres/repeated.py
@@ -1,18 +1,21 @@
 from datetime import datetime
+x = int(input())
 list = []
-i = input("Введите id: ")
-p = input("Количество: ")
-d = datetime.now().strftime('%Y-%m-%d')
-print("""Выберите статус:
+for i in range(x):
+	i = input("Введите id: ")
+	p = input("Количество: ")
+	d = datetime.now().strftime('%Y-%m-%d')
+	print("""Выберите статус:
 1 - Повтор
 2 - Обмен
 3 - Возврат
 """)
-j = int(input("Статус: "))
-ones = [' ',  'Повтор', 'Обмен', 'Возврат']
-word = ones[int(j)]
-list.append(f"({i}, '{word}', {p}, '{d}'),")
-print("""INSERT INTO
+	j = int(input("Статус: "))
+	ones = [' ',  'Повтор', 'Обмен', 'Возврат']
+	word = ones[int(j)]
+	list.append(f"({i}, '{word}', {p}, '{d}'),")
+	print("""INSERT INTO
     repeated (id, repeat, place, date)
 VALUES""")
-print(*[item + '\n' for item in list])
+	print(*[item + '\n' for item in list])
+	continue
