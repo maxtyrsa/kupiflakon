52390b450d1d968507fa66e64fa4ab65c38f6836
maxtyrsa <tyrsa@doctor.com>
2024-07-19 16:59:23 +0300

    dump base

--- script/git_pull_push.sh
+++ script/git_pull_push.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd
+pg_dump mydb > git/kupiflakon/postgres/kupiflakon.dump
 cd git/kupiflakon
 git pull
 git add *
 git commit * -m 'dump base'
 git push
+pg_ctl -D $PREFIX/var/lib/postgresql stop
